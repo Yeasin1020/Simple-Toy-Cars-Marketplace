4eaf39c3368262629e9e393bc0a74ea64fb97a17
Yeasin1st <ysbd111@gmail.com>
2023-05-31 21:52:39 +0600

    code source

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import router from './Routes/Routes.jsx';
 import AuthProvider from './provider/AuthProvider';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <div className='pt-5 px-1 lg:p-10 bg-gradient-to-r from-black via-purple-500 to-black '>
+  <div className='pt-5 px-1 lg:p-10 bg-gradient-to-r from-black via-purple-400 to-black '>
     <React.StrictMode>
      <AuthProvider>
      <RouterProvider router={router} />
